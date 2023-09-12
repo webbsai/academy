17008f9d9fde098e6d19a3c1259a4e146454c7af
Altaf Ahmad <altaf.ahmad23@gmail.com>
2023-09-12 10:03:08 +0500

    fix rss build issue

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -9,7 +9,9 @@ export async function get(context) {
 		description: SITE_DESCRIPTION,
 		site: context.site,
 		items: posts.map((post) => ({
-			...post.data,
+			title: post.data.title,
+			pubDate: post.data.pubDate,
+			description: post.data.description,
 			link: `/blog/${post.slug}/`,
 		})),
 	});

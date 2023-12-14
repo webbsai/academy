df415aa254f4bb34fb20b891532556aa034f0eca
sami <14bscssullah@seecs.edu.pk>
2023-12-13 22:35:51 -0800

    fix the bug and update packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,17 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import starlight from '@astrojs/starlight';
-
-import preact from '@astrojs/preact';
+import preact from "@astrojs/preact"
+import starlight from "@astrojs/starlight"
+import tailwind from "@astrojs/tailwind"
+import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: "https://academy.webbsai.com",
 	integrations: [
 		tailwind({}),
 		starlight({
-			title: 'WebbsAI',
-			customCss: ['./src/styles/custom.css', './src/styles/tailwind.css'],
+			title: "Academy ",
+			customCss: ["./src/styles/custom.css", "./src/styles/tailwind.css"]
 		}),
-		preact({ compat: true }),
-	],
-});
+		preact({ compat: true })
+	]
+})
